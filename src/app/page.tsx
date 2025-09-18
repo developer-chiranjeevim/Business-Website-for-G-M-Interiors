b4be8123ce4b3c6e87e4d94b5dd57ad26eefdf04
chiranjeevi <developer.chiranjeevim@gmail.com>
2025-09-18 10:42:47 +0530

    Feature: Added Whatsapp CTA Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,16 +14,22 @@ function Home(){
 
   return(
     <>
-      <Navbar />
-      <CarouselBanner />
-      {/* <Banner /> */}
-      <About />
-      <Portfolio />
-      <Services />
-      <TimelineSec />
-      <Reviews />
-      <Contact />
-      <Footer />
+      <div className="relative">
+        <Navbar />
+        <CarouselBanner />
+        {/* <Banner /> */}
+        <About />
+        <Portfolio />
+        <Services />
+        <TimelineSec />
+        <Reviews />
+        <Contact />
+        <Footer />
+
+        <a href="https://api.whatsapp.com/send/?phone=%2B919487288218&text&type=phone_number&app_absent=0" target="_blank" className="fixed z-30 bottom-5 right-5 bg-green-700 hover:bg-green-800 duration-300 rounded-full rounded-full p-4 cursor-pointer">
+          <img src="/icons/whatsapp.svg" alt="whatsapp call to action icon" className="w-auto h-[2rem]" />
+        </a>
+      </div>
     </>
   )
 };

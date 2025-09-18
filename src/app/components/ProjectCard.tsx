736ea4d47e97509f64fdeb4f72fe35673b3137c5
chiranjeevi <developer.chiranjeevim@gmail.com>
2025-09-18 10:02:29 +0530

    Fix: Added Title Text onHover in Portfolio Card Component

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -13,11 +13,14 @@ const ProjectCard : React.FC<CardProps> = ({image, title, description, location,
 
     return(
         <div className="w-full shadow-lg bg-white rounded-b-lg">
-            <div className="relative">
-                <img src={image} alt="" className="rounded-t-lg w-full h-full object-fill" />
-                <div className="opacity-0 duration-200 hover:opacity-100 absolute top-0 w-full h-full bg-gradient-to-t from-black to-transparent rounded-t-lg flex justify-center items-center">
-                    <div className="flex items-center gap-[1rem]">
-                        <div className="cursor-pointer w-fit px-[0.5rem] py-[0.25rem] bg-gray-200 rounded-full flex items-center gap-[0.5rem]">
+            <div className="relative cursor-pointer">
+                <img src={image} alt="" className="rounded-lg w-full h-full object-fill" />
+                <div className="opacity-0 duration-200 hover:opacity-100 absolute top-0 w-full h-full bg-gradient-to-t from-black to-transparent rounded-lg flex justify-start items-end">
+                    <div className="">
+                        <div className="p-[1rem]">
+                            <h1 className="text-white capitalize text-[1.5rem]">{title}</h1>
+                        </div>
+                        {/* <div className="cursor-pointer w-fit px-[0.5rem] py-[0.25rem] bg-gray-200 rounded-full flex items-center gap-[0.5rem]">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
@@ -29,7 +32,7 @@ const ProjectCard : React.FC<CardProps> = ({image, title, description, location,
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18 9 11.25l4.306 4.306a11.95 11.95 0 0 1 5.814-5.518l2.74-1.22m0 0-5.94-2.281m5.94 2.28-2.28 5.941" />
                             </svg>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
